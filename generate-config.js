947c1867e312d90f1aa84e206fabd34a0f12353c
breramon <breramon@gmail.com>
2026-05-21 07:40:14 -0300

    Correção exibição da chave de authenticator

---
+++ generate-config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const fs = require("fs");
+
+const content = `
+window.localConfig = {
+    firebase: {
+        apiKey: "${process.env.FIREBASE_API_KEY}",
+        authDomain: "${process.env.FIREBASE_AUTH_DOMAIN}",
+        projectId: "${process.env.FIREBASE_PROJECT_ID}",
+        storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+        appId: "${process.env.FIREBASE_APP_ID}",
+        measurementId: "${process.env.FIREBASE_MEASUREMENT_ID}"
+    }
+};
+`;
+
+fs.writeFileSync(
+    "./webapp/localconfig.js",
+    content
+);
--- webapp/util/Firebase.js
+++ webapp/util/Firebase.js
@@ -1,18 +1,13 @@
 // util/Firebase.js
 sap.ui.define([
-    "sap/ui/model/json/JSONModel"
-], function(JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "portalrbl/app/ui5/util/Config"
+], function(JSONModel,Config) {
     "use strict";
 
-    var firebaseConfig = {
-        apiKey: "AIzaSyD_r5ysuyDZ_TXnl13WDgkSwvnzyB1JA18",
-        authDomain: "portalrbl-ui5.firebaseapp.com",
-        projectId: "portalrbl-ui5",
-        storageBucket: "portalrbl-ui5.firebasestorage.app",
-        messagingSenderId: "87339604566",
-        appId: "1:87339604566:web:cca88eb0d683852191b9f7",
-        measurementId: "G-KS3CTWSQWR"
-    };
+    var firebaseConfig = Config.firebase;
+
+    console.log(firebaseConfig);
     
     var firebaseApp;
     
